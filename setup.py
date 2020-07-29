3ab0a2ea259fe7a63552eeccdb205c8dc6d792b0
Bob Fang <bob.fang.london@gmail.com>
2020-07-29 20:35:20 +0000

    init

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+import versioneer
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ostanes", # Replace with your own username
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Bob Fang",
+    author_email="Bob.Fang.London@gmail.com",
+    description="A package for unit testing sqlalchemy modules",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bobfang1992/ostanes",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
